b6963ef5ffcb829b02aa0af7aeaad7ba3c31039c
Shubhajeetgithub <61720452+Shubhajeetgithub@users.noreply.github.com>
2025-06-04 23:45:19 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -51,7 +51,10 @@ const app = express()
 // };
 
 // app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
 
 app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({extended: true, limit: "20kb"}))
