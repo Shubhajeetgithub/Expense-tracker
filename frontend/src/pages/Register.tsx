0a0a141d51ec59e0b2f7afd10f6de8ef11d7cfa6
Shubhajeetgithub <61720452+Shubhajeetgithub@users.noreply.github.com>
2025-06-04 23:03:20 +0530

    Update Register.tsx

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -18,7 +18,7 @@ function Register() {
     e.preventDefault();
     setIsLoading(true);
     const data: RegisterFormData = { email, fullName, password };
-    axios.post('http://localhost:8000/register', data)
+    axios.post('https://expense-tracker-server-nine-vert.vercel.app/register', data)
       .then((result: any) => {
         alert(result.data.message)
         if (result.data.message === "Success") navigate('/login');
